91794f67ae78ce1caaf1f7b8927b0d2b03daea82
leeyongmin <trex0723@naver.com>
2024-10-08 14:34:48 +0900

    1008chatjs

--- openai/chat.js
+++ openai/chat.js
@@ -1,22 +1,23 @@
-OPENAPI_KEY = ""
+OPENAPI_KEY = 
 
 smodel = "gpt-3.5-turbo"
 smodelmini = "gpt-4o-mini"
-squestion = "황진이가 누구지"
-function talk(){
+//squestion = "황진이가 누구야? 한국어로 대답해줘"
+   
+function draw(){
+
+    squestion = txtMsg.value
     data ={
-        model: smodelmini,
-        messages: [
-            {
-                role: "user",
-                content: "squestion"
-            }
-        ]
+        prompt: squestion,
+        n: 2,
+        size:"256x256"
+            
+
     }
 
     $.ajax({
         type:"POST",
-        url: "https://api.openai.com/v1/chat/completions",
+        url: "https://api.openai.com/v1/images/generations",
         headers:{
             "Authorization": "Bearer " + OPENAPI_KEY
         },
@@ -24,11 +25,15 @@ function talk(){
         contentType: "application/json; charset=utf-8"
     }).done( function(response){
         console.log(response)
-        alert(response.choices[0].message.content)
-    }).fail(
+        //alert(response.choices[0].message.content)
+        gimage.src = response.data[0].url
+        gimage2.src = response.data[1].url
+
+    }).fail(function(error){
+        console.log(error)
+        errormsg = error.status + " : " + error.responseJSON.error.code + " - " + error.responseJSON.error.message
+        txtOut.value = errormsg
+    }
 
     )
 }
-
-talk()
-
